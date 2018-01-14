290a5d69857d6778d0d4ee42e80a714f33a8efef
PoltarokovSP <psdevelop@yandex.ru>
2018-01-14 05:57:36 +0300

    синхронизация с версией для разработки

--- td_app/index.js
+++ td_app/index.js
@@ -15,7 +15,7 @@ var clientsCount = 0;
 var config = {
     user: 'app_server',
     password: 'app_server',
-    server: '192.168.0.90\\SQLEXPRESS', // You can use 'localhost\\instance' to connect to named instance 
+    server: '192.168.1.90\\SQLEXPRESS', // You can use 'localhost\\instance' to connect to named instance 
     database: 'TD5R1',
     
     options: {
