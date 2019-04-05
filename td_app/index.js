776950d4710406addf18d31b2d5eef1fe332d394
PoltarokovSP <psdevelop@yandex.ru>
2019-04-06 01:56:37 +0300

    localhost db addrr

--- td_app/index.js
+++ td_app/index.js
@@ -15,7 +15,7 @@ var clientsCount = 0;
 var config = {
     user: 'app_server',
     password: 'app_server',
-    server: '192.168.100.50\\SQLEXPRESS', // You can use 'localhost\\instance' to connect to named instance 
+    server: 'localhost\\SQLEXPRESS', // You can use 'localhost\\instance' to connect to named instance 
     database: 'TD5R1',
     
     options: {
